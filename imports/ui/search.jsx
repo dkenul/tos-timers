11545a9be28a623bc31624f6039858118a1d6d41
Damon Kenul <damon.kenul@gmail.com>
2016-06-03 00:52:22 -0400

    add state logic to search

--- imports/ui/search.jsx
+++ imports/ui/search.jsx
@@ -1,15 +1,44 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class Search extends Component {
+  constructor () {
+    super()
+    this.state = {
+      upFilter: false,
+      downFilter: false,
+      unknownFilter: false
+    }
+  }
+
+  addCheckboxes () {
+    let checkboxes = ['up', 'down', 'unknown'].map((el, i) => (
+      <div
+        id={el}
+        key={i}
+        className={this.state[`${el}Filter`] ? "clicked" : ""}
+        onClick={this.handleClick.bind(this, el)}
+      />
+    ))
+
+    return (
+      <div className="checkboxes group">
+        {checkboxes}
+      </div>
+    )
+  }
+
+  handleClick (el) {
+    let state = {}
+    let key = `${el}Filter`
+    state[key] = !this.state[key]
+    this.setState(state)
+  }
+
   render () {
     return (
       <form className="group">
         <input type="text" name="search" id="search" autocomplete="off" />
-        <div className="checkboxes group">
-          <div id="up" />
-          <div id="down" />
-          <div id="unknown" />
-        </div>
+        {this.addCheckboxes()}
       </form>
     )
   }
