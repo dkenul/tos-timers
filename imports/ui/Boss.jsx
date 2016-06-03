1133e837e562db9481d232a33970f994f214b6f3
Damon Kenul <damon.kenul@gmail.com>
2016-06-03 02:45:00 -0400

    add dropdown logic

--- imports/ui/Boss.jsx
+++ imports/ui/Boss.jsx
@@ -1,20 +1,35 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class Boss extends Component {
+  constructor () {
+    super()
+    this.state = {
+      showDropdown: false
+    }
+  }
+
+  toggleDropdown () {
+    this.setState({showDropdown: !this.state.showDropdown})
+  }
+
   render () {
+    let dropdown =
+      <ul className="dropdown">
+        <li className="channel">1</li>
+        <li className="channel">2</li>
+        <li className="channel">3</li>
+        <li className="channel">4</li>
+        <li className="channel">5</li>
+      </ul>
+
     return (
       <li>
-        <div className="boss group">
+        <div className="boss group" onClick={this.toggleDropdown.bind(this)}>
           <img src="/images/earth_archon.jpg" alt="" />
           <div className="name">{this.props.boss.name}</div>
         </div>
-        <ul className="dropdown">
-          <li className="channel">1</li>
-          <li className="channel">2</li>
-          <li className="channel">3</li>
-          <li className="channel">4</li>
-          <li className="channel">5</li>
-        </ul>
+
+        {this.state.showDropdown ? dropdown : ""}
       </li>
     )
   }
